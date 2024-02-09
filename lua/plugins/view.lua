0a7e17b2e07ab5c42ce9f9f475b374fb8956bec2
Rafael Prado <14060@sarah.br>
2024-02-09 14:42:48 -0300

    tabs and windows

--- lua/plugins/view.lua
+++ lua/plugins/view.lua
@@ -40,5 +40,67 @@ return {
       --   If not available, we use `mini` as the fallback
       "rcarriga/nvim-notify",
     }
-  }
+  },
+  -- {
+  --   {
+  --     'romgrk/barbar.nvim',
+  --     dependencies = {
+  --       'lewis6991/gitsigns.nvim',     -- OPTIONAL: for git status
+  --       'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+  --     },
+  --     init = function() vim.g.barbar_auto_setup = false end,
+  --     opts = {
+  --       -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+  --       -- animation = true,
+  --       -- insert_at_start = true,
+  --       -- …etc.
+  --     },
+  --     version = '^1.0.0', -- optional: only update when a new 1.x version is released
+  --     config = function()
+  --       require("barbar").setup()
+  --     end
+  --   },
+  -- },
+  -- Tabs
+  {
+    'kdheepak/tabline.nvim',
+    config = function()
+      require 'tabline'.setup {
+        -- Defaults configuration options
+        enable = true,
+        options = {
+          -- If lualine is installed tabline will use separators configured in lualine by default.
+          -- These options can be used to override those settings.
+          section_separators = { '', '' },
+          component_separators = { '', '' },
+          max_bufferline_percent = 66, -- set to nil by default, and it uses vim.o.columns * 2/3
+          show_tabs_always = false,    -- this shows tabs only when there are more than one tab or if the first tab is named
+          show_devicons = true,        -- this shows devicons in buffer section
+          show_bufnr = false,          -- this appends [bufnr] to buffer section,
+          show_filename_only = false,  -- shows base filename only instead of relative path in filename
+          modified_icon = "+ ",        -- change the default modified icon
+          modified_italic = false,     -- set to true by default; this determines whether the filename turns italic if modified
+          show_tabs_only = false,      -- this shows only tabs instead of tabs + buffers
+        }
+      }
+      vim.cmd [[
+      set guioptions-=e " Use showtabline in gui vim
+      set sessionoptions+=tabpages,globals " store tabpages and globals in session
+    ]]
+    end,
+    requires = { { 'hoob3rt/lualine.nvim', opt = true }, { 'kyazdani42/nvim-web-devicons', opt = true } }
+  },
+{
+  "anuvyklack/windows.nvim",
+  dependencies = {
+    "anuvyklack/middleclass",
+    "anuvyklack/animation.nvim"
+  },
+  config = function()
+    vim.o.winwidth = 10
+    vim.o.winminwidth = 10
+    vim.o.equalalways = false
+    require('windows').setup()
+  end
+}
 }
